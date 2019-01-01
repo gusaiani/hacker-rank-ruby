418efa678e88544e4cf5a23256c2c3fb03f2743b
Gustavo Saiani <gustavo@poe.ma>
2019-01-01 11:53:22 -0200

    Solve Design PDF Viewer

---
+++ spec/design_pdf_viewer_spec.rb
@@ -0,0 +1,27 @@
+require 'design_pdf_viewer'
+
+RSpec.describe 'design_pdf_viewer' do
+  it 'returns 9' do
+    character_heights = [
+      1, 3, 1, 3, 1, 4, 1, 3, 2,
+      5, 5, 5, 5, 5, 5, 5, 5, 5,
+      5, 5, 5, 5, 5, 5, 5, 5
+    ]
+
+    word = 'abc'
+
+    expect(design_pdf_viewer(character_heights, word)).to eq 9
+  end
+
+  it 'returns 9' do
+    character_heights = [
+      1, 3, 1, 3, 1, 4, 1, 3, 2,
+      5, 5, 5, 5, 5, 5, 5, 5, 5,
+      5, 5, 5, 5, 5, 5, 5, 7
+    ]
+
+    word = 'zaba'
+
+    expect(design_pdf_viewer(character_heights, word)).to eq 28
+  end
+end
